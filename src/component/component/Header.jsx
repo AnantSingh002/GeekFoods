e612e20b6ad9f4ce2543cd8b083ce6f7b095a76e
Anant <anantsinghchouhan@gmail.com>
2025-04-13 20:28:33 +0530

    resolved image paths

--- src/component/component/Header.jsx
+++ src/component/component/Header.jsx
@@ -7,6 +7,7 @@ function Header() {
     <header>
     <div className={style.container}>
         <div className={style.logo}>
+          <img src="/home/logo.svg" alt="" srcset="" />
           <h1>GeekFood</h1>
         </div>
         <div className={style.list}>
@@ -28,14 +29,15 @@ function Header() {
     <div className={style.content}>
       <div className={style.box}>
         <h1>Let Us Find Your</h1>
-        <h2>Forever Food</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-        <div className='Btn'>
-          <button>Search Now</button>
-          <button>Know More</button>
+        <h2>Forever Food...</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.. </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, maiores!</p>
+        <div className={style.btnn}>
+          <button id={style.search}>Search Now</button>
+          <button id={style.knowMore}>Know More</button>
         </div>
       </div>
-      <img src="/src/assets/home.jpeg" alt="" srcset="" />
+      <img src="/home/home.jpeg" alt="" srcset="" />
 
     </div>
 
@@ -43,19 +45,138 @@ function Header() {
 
 
     <div className={style.house}>
-      <img src="/src/assets/house.jpeg" alt="" srcset=""/>
+      <img src="/home/house.jpeg" alt="" srcset=""/>
     </div>
 
     <div className={style.description}>
       <div>
       <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, debitis.</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, molestiae! Quidem est esse numquam odio deleniti, beatae, magni dolores provident quaerat totam eos, aperiam architecto eius quis quibusdam fugiat dicta.</p>
-      <button>Get In Touch</button>
+      <button id={style.inTouch}>Get In Touch</button>
       </div>
 
     </div>
 
     </div>
+
+    <div className={style.masonry}>
+      <div className={style.card} id={style.c1}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis perferendis libero est veritatis similique tempora. Cum quis pariatur dolorem obcaecati illo asperiores odio mollitia iste aspernatur sequi molestias, voluptatem beatae esse. Et libero placeat itaque voluptas ut saepe natus. Ullam similique, aperiam nemo neque magni reiciendis sequi iure earum autem!</p>
+        
+        <div className={style.subDiv}>
+          <img src="/home/dp.jpeg"/>
+
+          <div className={style.name}>
+          <h3>Gladis Lennon</h3>
+          <span>Head of SEO</span>
+          </div>
+        </div>
+      </div>
+
+
+      <div className={style.card} id={style.c2}>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim quisquam, architecto recusandae culpa ipsum esse deleniti alias facere? Ipsum, inventore.</p>
+        <div className={style.subDiv}>
+          <img src="/home/dp.jpeg"/>
+
+          <div className={style.name}>
+          <h3>Gladis Lennon</h3>
+          <span>Head of SEO</span>
+          </div>
+        </div>
+
+
+      </div>
+
+
+      <div className={style.card} id={style.c3}>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim quisquam, </p>
+      <div className={style.subDiv}>
+          <img src="/home/dp.jpeg"/>
+
+          <div className={style.name}>
+          <h3>Gladis Lennon</h3>
+          <span>Head of SEO</span>
+          </div>
+        </div>
+
+
+      </div>
+
+
+      <div className={style.card} id={style.c4}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis perferendis libero est veritatis similique tempora. Cum quis pariatur dolorem obcaecati illo asperiores odio mollitia iste aspernatur sequi molestias, voluptatem beatae esse. </p>
+        
+        <div className={style.subDiv}>
+          <img src="/home/dp.jpeg"/>
+
+          <div className={style.name}>
+          <h3>Gladis Lennon</h3>
+          <span>Head of SEO</span>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className={style.card} id={style.c5}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis perferendis libero est veritatis similique tempora. Cum quis pariatur dolorem obcaecati illo  </p>
+        
+        <div className={style.subDiv}>
+          <img src="/home/dp.jpeg"/>
+
+          <div className={style.name}>
+          <h3>Gladis Lennon</h3>
+          <span>Head of SEO</span>
+          </div>
+        </div>
+      
+      </div>
+
+
+      <div className={style.card} id={style.c6}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis perferendis libero est veritatis similique tempora. </p>
+        
+        <div className={style.subDiv}>
+          <img src="/home/dp.jpeg"/>
+
+          <div className={style.name}>
+          <h3>Gladis Lennon</h3>
+          <span>Head of SEO</span>
+          </div>
+        </div>
+      </div>
+      
+ 
+    </div>
+
+    <footer>
+    <div>
+     <img src="/home/footereLogo.svg" alt="" srcset="" />    
+    </div>
+
+    <div>
+    <p>Lorem ipsum dolor sit amet. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, corporis.</p>
+    <br />
+    <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+    </div>
+
+    <div className={style.des}>
+    <p>About</p>
+    <p>Carrer</p>
+    <p>History</p>
+    <p>Service</p>
+    <p>Blog</p>
+    </div>
+
+    <div className={style.icons}>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    </div>
+
+    </footer>
 
     
 
